8bb291884b6501d88183b148b462246fbcbdceac
Bird Eggegg <birdeggegg@gmail.com>
2015-08-29 23:09:51 +0800

    make import first

--- generators/app/templates/_main.js
+++ generators/app/templates/_main.js
@@ -1,18 +1,15 @@
-import Vue from 'vue<% if (includeResource) { %>
-
-import vueResource from 'vue-resource'
-Vue.use(vueResource)<% } %>
-
-Vue.config.debug = process.env.NODE_ENV !== 'production'<% if (includeRouter) { %>
-
+import Vue from 'vue'<% if (includeResource) { %>
+import vueResource from 'vue-resource'<% } %><% if (includeRouter) { %>
 import VueRouter from 'vue-router'
-import { configRouter } from './router-config'
-Vue.use(VueRouter)
+import { configRouter } from './router-config'<% } %>
+Vue.config.debug = process.env.NODE_ENV !== 'production'<% if (includeResource) { %>
 
+Vue.use(vueResource)<% } %><% if (includeRouter) {%>
+
+Vue.use(VueRouter)
 const router = new VueRouter({
 })
 configRouter(router)
-
 const App = Vue.extend(require('./app.vue'))
 router.start(App, '#app')<% } %><% if (!includeRouter) { %>
 const App = new Vue(require('./app.vue'))<% } %>
